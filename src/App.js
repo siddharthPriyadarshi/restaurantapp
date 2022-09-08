7ab21c34a3732bea7259cdbb414d8f9d23a5d305
Siddharth Priyadarshi <sidmail4606@gmail.com>
2022-09-08 18:07:16 +0530

    spin up react-app with necessary dependencies

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className = "w-screen h-screen flex items-center justify-center text-blue-600">App</div>
+  )
+}
+
+export default App
