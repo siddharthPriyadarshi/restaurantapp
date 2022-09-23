1295bb9ef147fb95c777378d00ba82bd712e5e98
Siddharth Priyadarshi <sidmail4606@gmail.com>
2022-09-23 17:44:06 +0530

    fix: dropdown animation added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import { MdShoppingBasket, MdAdd, MdLogout } from 'react-icons/md'
 import { motion } from "framer-motion";
 
@@ -18,6 +18,8 @@ const Header = () => {
 
     const [{ user }, dispatch] = useStateValue();
 
+    const [isMenu, setIsMenu] = useState(false);
+
     const login = async () => {
         if (!user) {
             const { user: { refreshToken, providerData } } = await signInWithPopup(firebaseAuth, provider);
@@ -25,8 +27,9 @@ const Header = () => {
                 type: actionType.SET_USER,
                 user: providerData[0]
             });
-
             localStorage.setItem('user', JSON.stringify(providerData[0]));
+        }else{
+            setIsMenu(!isMenu);
         }
 
     };
@@ -63,10 +66,24 @@ const Header = () => {
                             onClick={login}
                         />
 
-                        <div className="w-40 bg-gray-50 shadow-xl rounded-xl flex flex-col absolute top-12 right-0">  
-                            <p className = "px-5 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
-                            <p className = "px-5 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /></p>
-                        </div>
+                        {
+                            isMenu && (
+                                <motion.div initial={{ opacity: 0, scale: 0.6 }}
+                                            animate={{ opacity: 1, scale: 1 }}
+                                            exit={{ opacity: 0, scale: 0.6 }}
+                                 className="w-40 bg-gray-50 shadow-xl rounded-xl flex flex-col absolute top-12 right-0">
+                                    {
+                                        user && user.email === "devislife4606@gmail.com" && (
+                                            <Link to={"/createItem"}>
+                                                <p className="px-5 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
+                                            </Link>
+                                        )
+                                    }
+                                    <p className="px-5 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /></p>
+                                </motion.div>
+                            )
+                        }
+
                     </div>
                 </div>
             </div>
