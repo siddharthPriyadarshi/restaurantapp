9a916138c33a67da246b1c68d0b8b051488c39bf
Siddharth Priyadarshi <sidmail4606@gmail.com>
2022-11-20 14:04:40 +0530

    feat: Logout feat added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,12 +25,25 @@ const Header = () => {
             const { user: { refreshToken, providerData } } = await signInWithPopup(firebaseAuth, provider);
             dispatch({
                 type: actionType.SET_USER,
-                user: providerData[0]
+                user: providerData[0],
             });
             localStorage.setItem('user', JSON.stringify(providerData[0]));
         } else {
             setIsMenu(!isMenu);
         }
+
+    };
+
+    const logout = () => {
+        // Hide the menu, update context or state and clear the local storage
+        setIsMenu(false);
+        localStorage.clear();
+        dispatch({
+            type : actionType.SET_USER,
+            user : null,
+        });
+         
+
 
     };
 
@@ -75,13 +88,13 @@ const Header = () => {
                                     exit={{ opacity: 0, scale: 0.6 }}
                                     className="w-40 bg-gray-50 shadow-xl rounded-xl flex flex-col absolute top-12 right-0">
                                     {
-                                        user && user.email === "devislife4606@gmail.com" && (
+                                        user && user.email === "development.testmail123@gmail.com" && (
                                             <Link to={"/createItem"}>
-                                                <p className="px-5 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
+                                                <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
                                             </Link>
                                         )
                                     }
-                                    <p className="px-5 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /></p>
+                                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base" onClick = {logout}>Logout <MdLogout /></p>
                                 </motion.div>
                             )
                         }
@@ -115,7 +128,7 @@ const Header = () => {
                                 exit={{ opacity: 0, scale: 0.6 }}
                                 className="w-40 bg-gray-50 shadow-xl rounded-xl flex flex-col absolute top-12 right-0">
                                 {
-                                    user && user.email === "devislife4606@gmail.com" && (
+                                    user && user.email === "development.testmail123@gmail.com" && (
                                         <Link to={"/createItem"}>
                                             <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
                                         </Link>
@@ -127,7 +140,8 @@ const Header = () => {
                                     <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out hover:bg-slate-100 px-4 py-2">Service</li>
                                     <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out hover:bg-slate-100 px-4 py-2">Home</li>
                                 </ul>
-                                <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base justify-center">Logout <MdLogout /></p>
+                                <p className="m-2 p-2 rounded-md shadow-md flex items-center bg-gray-200 gap-3 cursor-pointer hover:bg-slate-300 transition-all duration-100 ease-in-out text-textColor text-base justify-center"
+                                onClick = {logout}>Logout <MdLogout /></p>
 
 
 
