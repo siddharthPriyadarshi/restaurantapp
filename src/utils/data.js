fe702551e004d78ef459cc5de5d4d9c17e297ec1
Siddharth Priyadarshi <sidmail4606@gmail.com>
2022-12-04 15:32:42 +0530

    Home container and mobile view updated

---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import I1 from '../img/i1.png'
+import F1 from '../img/f1.png'
+import C1 from '../img/c1.png'
+import Fi1 from '../img/fi1.png'
+
+export const heroData = [
+  {
+    id: 1,
+    name: 'IceCream',
+    desc: 'Chocolate & Vanilla',
+    price: '35',
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: 'Strawberries',
+    desc: 'Fresh Strawberries',
+    price: '40',
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: 'Chicken Kebab',
+    desc: 'Mixed Kebab Plate',
+    price: '89',
+    imageSrc: C1,
+  },
+  {
+    id: 4,
+    name: 'Fish Kebab',
+    desc: 'Mixed Fish Kebab',
+    price: '99',
+    imageSrc: Fi1,
+  },
+]
